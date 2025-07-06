e5b0ec508bd4c87e8bb43314234a2ac92269b086
vidyasagarika <159220543+vidyasagarika@users.noreply.github.com>
2025-07-06 12:25:51 +0530

    Create user_configs.py

---
+++ user_configs.py
@@ -0,0 +1,35 @@
+class gesture_binds:
+    actions = {
+        # Mouse Functionality
+        'open_index_middle': 'cursor',      # DNC
+        'combine_index_middle': 'click',    # DNC
+
+        # General Functionality
+        'fold_thumb': 'open_browser',
+        'open_index': 'paste',
+        'open_ring': 'screenshot',
+        'open_pinky': 'open_explorer',
+
+        # Media Controls (YouTube)
+        'open_thumb': 'play_pause_vid',
+        'fold_index': 'rewind_vid',
+        'fold_middle': 'forward_vid',
+        'fold_ring': 'open_cmd',
+        'fold_pinky': 'full_screen',
+
+        # Open-Program Controls
+        'fold_thumb_and_index': 'open_vscode',
+        'fold_middle_and_ring': 'open_spotify',
+    }
+
+
+class paths:
+    program_paths = {
+        'desktop': "C:/Users/DELL 3400/Desktop",
+        'vscode': "C:/Users/DELL 3400/AppData/Local/Programs/Microsoft VS Code/Code.exe",
+        # 'discord': "C:/Users/DELL 3400/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Discord Inc/Discord.lnk",
+        'browser': "C:/Program Files/Google/Chrome/Application/chrome.exe",
+        'cmd': "C:/Windows/System32/cmd.exe",
+        'explorer': "C:/Windows/explorer.exe",
+        'spotify': "C:/Users/DELL 3400/Downloads/Spotify - Music and Podcasts Installer.exe"
+    }
